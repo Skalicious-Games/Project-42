d2bc97badfa95cec6658879fa4a6e28ecbe6c0ac
myerkes <mattyerkes23@gmail.com>
2015-06-30 19:58:48 -0500

    Added packages and example components and entitysystem

---
+++ core/src/components/PositionComponent.java
@@ -0,0 +1,12 @@
+/*
+ * Component that connects x and y variables for the position of an entity
+ */
+
+package components;
+
+import com.badlogic.ashley.core.Component;
+
+public class PositionComponent extends Component {
+	public float x = 0.0f;
+	public float y = 0.0f;
+}
---
+++ core/src/components/VelocityComponent.java
@@ -0,0 +1,12 @@
+/*
+ * Component that contains x and y variables for speeds in x and y directions of an entity
+ */
+
+package components;
+
+import com.badlogic.ashley.core.Component;
+
+public class VelocityComponent extends Component {
+	public float x = 0.0f;
+	public float y = 0.0f;
+}
